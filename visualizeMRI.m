8c80495961541142ef199e4f0d1155da16123a59
Joel Smith <joelsmith2004@gmail.com>
2024-05-23 10:54:13 +0000

    23 May 2024, 18:54

--- visualizeMRI.m
+++ visualizeMRI.m
@@ -2,7 +2,7 @@ function visualizeMRI
     
     fig = uifigure('Name', 'MRI Analyzer', 'Position', [100 100 800 500]);
 
-    loadDirButton = uibutton(fig, 'Text', 'Load Slice Directory', 'Position', [50 450 150 30], 'ButtonPushedFcn', @loadSliceDirectory); %#ok<*NASGU>
+    loadDirButton = uibutton(fig, 'Text', 'Load Slice Directory', 'Position', [50 450 150 30], 'ButtonPushedFcn', @loadSliceDirectory);
     channelDropdownLabel = uilabel(fig, 'Text', 'Channel:', 'Position', [250 480 60 15]);
     channelDropdown = uidropdown(fig, 'Items', {'T1', 'T1Gd', 'T2', 'T2-FLAIR'}, 'Position', [250 450 100 30], 'ValueChangedFcn', @changeChannel);
     annotationDropdownLabel = uilabel(fig, 'Text', 'Annotation:', 'Position', [400 480 80 15]);
